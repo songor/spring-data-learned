102a1095f837974a33e4ce82c5fff9936efb7b4f
songor <chensongyu1028@gmail.com>
2021-08-11 09:56:50 +0800

    Spring Data JPA

--- spring-data/src/main/java/org/example/spring/data/repository/EmployeeRepository.java
+++ spring-data/src/main/java/org/example/spring/data/repository/EmployeeRepository.java
@@ -1,9 +1,34 @@
 package org.example.spring.data.repository;
 
+import java.util.List;
 import org.example.spring.data.domain.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends Repository<Employee, Integer> {
 
   Employee findByName(String name);
+
+  List<Employee> findByNameStartingWithAndAgeLessThan(String name, Integer age);
+
+  List<Employee> findByNameInOrAgeLessThan(List<String> names, Integer age);
+
+  @Query("select e from Employee e where e.id = (select max(id) from Employee)")
+  Employee getLatestEmployee();
+
+  @Query("select e from Employee e where e.name=?1 and e.age=?2")
+  List<Employee> getEmployeeByIndex(String name, Integer age);
+
+  @Query("select e from Employee e where e.name=:name and e.age=:age")
+  List<Employee> getEmployeeByParam(@Param("name") String name, @Param("age") Integer age);
+
+  @Query("select e from Employee e where e.name like %?1%")
+  List<Employee> getEmployeeByIndexNameFuzzy(String name);
+
+  @Query("select e from Employee e where e.name like %:name%")
+  List<Employee> getEmployeeByParamNameFuzzy(@Param("name") String name);
+
+  @Query(nativeQuery = true, value = "select count(1) from employee")
+  Long count();
 }
--- spring-data/src/test/java/org/example/spring/data/repository/EmployRepositoryTest.java
+++ spring-data/src/test/java/org/example/spring/data/repository/EmployRepositoryTest.java
@@ -1,7 +1,10 @@
 package org.example.spring.data.repository;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
 import org.example.spring.data.domain.Employee;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,5 +19,89 @@ public class EmployRepositoryTest {
     Employee employee = repository.findByName("a");
     System.out.println(employee);
     assertNotNull(employee);
+  }
+
+  @Test
+  public void testFindByNameStartingWithAndAgeLessThan() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    List<Employee> employee = repository.findByNameStartingWithAndAgeLessThan("a", 30);
+    for (Employee e : employee) {
+      System.out.println(e);
+    }
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testFindByNameInOrAgeLessThan() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    List<Employee> employee = repository.findByNameInOrAgeLessThan(Arrays.asList("a", "b"), 30);
+    for (Employee e : employee) {
+      System.out.println(e);
+    }
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testGetLatestEmployee() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    Employee employee = repository.getLatestEmployee();
+    System.out.println(employee);
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testGetEmployeeByIndex() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    List<Employee> employee = repository.getEmployeeByIndex("a", 18);
+    for (Employee e : employee) {
+      System.out.println(e);
+    }
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testGetEmployeeByParam() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    List<Employee> employee = repository.getEmployeeByParam("a", 18);
+    for (Employee e : employee) {
+      System.out.println(e);
+    }
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testGetEmployeeByIndexNameFuzzy() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    List<Employee> employee = repository.getEmployeeByIndexNameFuzzy("a");
+    for (Employee e : employee) {
+      System.out.println(e);
+    }
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testGetEmployeeByParamNameFuzzy() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    List<Employee> employee = repository.getEmployeeByParamNameFuzzy("a");
+    for (Employee e : employee) {
+      System.out.println(e);
+    }
+    assertNotNull(employee);
+  }
+
+  @Test
+  public void testCount() {
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans-jpa.xml");
+    EmployeeRepository repository = context.getBean(EmployeeRepository.class);
+    Long count = repository.count();
+    System.out.println("count: " + count);
+    assertNotNull(count);
   }
 }
